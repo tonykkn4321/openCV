eafabd86c8fd2ee6db7d8221a2b698177e6898d5
Tony Kwong <tonykkn4321@gmail.com>
2025-06-29 10:42:31 +0800

    Completed Drawing Functions

--- 1 - Gui Features in OpenCV/1.2 - Getting Started with Videos/video_save.py
+++ 1 - Gui Features in OpenCV/1.2 - Getting Started with Videos/video_save.py
@@ -9,7 +9,9 @@ out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (640,480))
 while(cap.isOpened()):
     ret, frame = cap.read()    
     if ret==True:
-        frame = cv2.flip(frame,0)
+
+        # Flip vertically (upside down)
+        frame = cv2.flip(frame,0) 
 
         # write the flipped frameQ
         out.write(frame)
---
+++ 1 - Gui Features in OpenCV/1.3 - Drawing Functions in OpenCV/ellipse.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+
+# Create a white image
+#img = np.ones((512,512,3), np.uint8)*255
+
+# Draw a diagonalblue line with thickness of 5 px
+img = cv2.line(img,(0,0),(511,511),(255,0,0),5)
+
+# To draw a rectangle, you need top-left corner and bottom-right corner of rectangle. 
+# This time we will draw a green  rectangle at the top-right corner of image.
+img = cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
+
+# To draw a circle, you need its center coordinates and radius.
+# We will draw a circle inside the rectangle drawn above.
+img = cv2.circle(img,(447,63), 63, (0,0,255),-1)
+
+# Draw a half ellipse at the center of the image
+img = cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+
+cv2.imshow("image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
