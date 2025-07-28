31e3c41b8dd3a19e7af2c08b972f3b6498f82b9f
Tony Kwong <tonykkn4321@gmail.com>
2025-07-28 12:02:53 +0800

    Completed 3.4 - Smoothing Images

--- 3 -  Image Processing in OpenCV/3.4 -  Smoothing Images/ImageBlurring_BilateralFiltering.py
+++ 3 -  Image Processing in OpenCV/3.4 -  Smoothing Images/ImageBlurring_BilateralFiltering.py
@@ -4,10 +4,13 @@ from matplotlib import pyplot as plt
 
 img=cv2.imread('Bilateral-Filtering-Image.jpg')
 
+# OpenCV uses BGR format by default, while matplotlib.pyplot.imshow() expects RGB.
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 blur = cv2.bilateralFilter(img,9,75,75)
+blur_rgb = cv2.cvtColor(blur, cv2.COLOR_BGR2RGB)
 
-plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.subplot(121),plt.imshow(img_rgb),plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.subplot(122),plt.imshow(blur_rgb),plt.title('Blurred')
 plt.xticks([]), plt.yticks([])
 plt.show()
